275921e144f10d64a566d93d83b2fb50f08f65e1
Alexey Zakruzhetskyi <l500etc@gmail.com>
2020-01-26 14:44:20 +0200

    add user's component, model and service

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  id: number;
+  // tslint:disable-next-line:variable-name
+  first_name: string;
+  // tslint:disable-next-line:variable-name
+  last_name: string;
+  email: string;
+  region: string;
+  image: string;
+  sex: string;
+  subscription: boolean;
+  // tslint:disable-next-line:variable-name
+  additional_information: string;
+}
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../core/models/user.model';
+import { UsersService } from '../core/services/users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit, OnDestroy {
+  users: User[];
+  subscription: Subscription;
+
+  constructor(private usersService: UsersService) { }
+
+  userInformation(e): any {
+    alert('Unavailable yet');
+    e.preventDefault();
+  }
+
+  ngOnInit() {
+    this.subscription = this.usersService.getUsers('https://create-users-api.herokuapp.com/users')
+      .subscribe(users => {
+        this.users = users;
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+}
